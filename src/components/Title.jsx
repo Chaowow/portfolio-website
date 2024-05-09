88468f1de48bb8ab1e06139f41c321bd1dae6a05
Frankie <frankiejosefej@gmail.com>
2024-05-09 11:21:41 -0400

    Finishing touches and implemented dark mode

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -5,7 +5,7 @@ function Title({ children, id }) { //eslint-disable-line
     <h1
         id={id && id}
         className="text-2xl font-bold underline underline-offset-8
-        decoration-4 mb-5 text-stone-900"
+        decoration-4 mb-5 text-stone-900 dark:text-white"
     >
         {children}
     </h1>
